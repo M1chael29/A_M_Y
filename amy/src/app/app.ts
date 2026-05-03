f6f2abf10b065a564620ac6c5717ab5d53b72fd3
Lulu-tesla <alejandrovibritania@gmail.com>
2026-05-03 14:49:44 -0400

    describe tus cambios

--- amy/src/app/app.ts
+++ amy/src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
+// Importamos la biblioteca de animaciones AOS
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,21 @@ import { Footer } from './layout/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amy-digital-agency';
+
+  ngOnInit(): void {
+    /*
+       Inicializamos AOS con una configuración Pro:
+       - duration: 1000ms para que sea suave.
+       - once: true para que no se repita la animación al subir.
+       - easing: 'ease-in-out-quart' para una curva de movimiento premium.
+    */
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: 'ease-in-out-quart',
+      delay: 100,
+    });
+  }
 }
